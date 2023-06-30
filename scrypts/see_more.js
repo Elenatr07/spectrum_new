8440fc6bb78bbde5e704ccb8e68a1d3fe07ebc90
Elenatr07 <e.turkina2010@yandex.ru>
2023-06-30 14:53:09 +1000

    12

--- scrypts/see_more.js
+++ scrypts/see_more.js
@@ -2,14 +2,18 @@
 function showless(){
     let btnText = document.getElementById("load_btn");
     let els = document.getElementsByClassName("_hidden");
+   var arrow = document.getElementById("load_btn");
+   
+   
     
     for (var i = 0, el; el = els[i]; i++){
       if (getComputedStyle(el).display == 'none') {
-        btnText.innerHTML = "See less";
+        btnText.innerHTML = "See Less";
         el.style.display = 'block';
+        arrow.classList.toggle('tool');
         
       } else {
-        btnText.innerHTML = "See more";
+        btnText.innerHTML = "See More";
         el.style.display = "none";
       }
    //console.log(els);
